39e2e2d94d499dbee67efac748ce464e8b13ba1b
Tigrina <karina6891@mail.ru>
2018-11-20 01:54:32 +0300

    добавила progress_bar

--- bootsy.js
+++ bootsy.js
@@ -1,13 +1,30 @@
 
 $(function () {
-  $('.carousel').carousel({
+	let countItems = $('.carousel-item').length;
+	let percents=(1/countItems)*100;
+	
+	$('.carousel').carousel({
     interval: false
   }).swipe( {
 		swipeLeft: function() {
 			$(this).carousel("next");
+			let currentState = $('.progress-bar').width() / $('.progress').parent().width() * 100;
+			if(currentState >= 99){
+				$('.progress-bar').css('width', '1%');
+			}else{
+				$('.progress-bar').css('width', (currentState + percents) +'%');
+			}
+			
 		},
 		swipeRight: function() {
 			$(this).carousel("prev");
+			let currentState = $('.progress-bar').width() / $('.progress').parent().width() * 100;
+			if(currentState <= 1){
+				$('.progress-bar').width('100%');
+			}else{
+				$('.progress-bar').css('width', (currentState - percents) +'%');
+			}
+			
 		},
 		allowPageScroll: "vertical"
 	});
